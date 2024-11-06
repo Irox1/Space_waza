850d70f3f6abd867447d028f86cbd71706f83d32
Loevan <loevanirox@gmail.com>
2024-11-06 23:45:38 +0100

    Tout sauf collision

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
-use minifb::{Key, Window, WindowOptions};
+use minifb::{Key, KeyRepeat, Window, WindowOptions};
+use rand::Rng;
 
+struct Ennemy {
+    x : f32,
+    y : f32,
+    width : f32,
+    height : f32,
+    dy : f32
+
+}
+
+impl Ennemy {
+    pub fn draw(&self, buf : &mut Vec<u32>, width_screen: &u32){
+        draw_rect(buf, self.x as u32, self.y as u32, width_screen, self.width as u32, self.height as u32, 0x1b8024);
+    }
+}
 struct Vaisseau {
     x : f32,
     y : f32,
@@ -14,7 +29,15 @@ impl Vaisseau {
         draw_rect(buf, self.x as u32, self.y as u32, width_screen, self.width, self.height, 0x000000);
     }
 
-    //pub fn update(&mut self, window : &Window, width_screen: &u32) {}
+    pub fn update(&mut self, width_screen: u32) {
+        self.x += self.dx;
+
+        if self.x < 0.0 {
+            self.x = width_screen as f32;
+        } else if self.x > width_screen as f32 {
+            self.x = 0.0;
+        }
+    }
 }    
 
 fn draw_pixel(buf: &mut Vec<u32>, x: u32, y: u32, width: &u32, colour : u32) {
@@ -36,12 +59,14 @@ fn draw_rect(buf : &mut Vec<u32>, x : u32, y : u32, width_screen: &u32, width : 
 struct Missile {
     x : f32,
     y : f32,
+    width : u32,
+    height : u32,
 }
 
 impl Missile {
 
     pub fn draw(&self, buf : &mut Vec<u32>, width_screen: &u32) {
-        draw_rect(buf, self.x as u32, self.y as u32, width_screen, 10, 20, 0x5DE2E7)
+        draw_rect(buf, self.x as u32, self.y as u32, width_screen, self.width, self.height, 0x5DE2E7)
     }
 }
 
@@ -51,13 +76,16 @@ struct Jeu {
     buffer : Vec<u32>,
     window : Window,
     fps : usize,
+    space_pressed :bool,
+    frame_count : u32,
     player : Vaisseau,
-    liste_tirs : Vec<Missile>
+    liste_tirs : Vec<Missile>,
+    liste_ennemy : Vec<Ennemy>
 }
 
 impl Jeu {
 
-    pub fn new(width: u32, height: u32, fps: usize, player: Vaisseau, liste_tirs: Vec<Missile>) -> Self {
+    pub fn new(width: u32, height: u32, fps: usize, player: Vaisseau, liste_tirs: Vec<Missile>, liste_ennemy : Vec<Ennemy>) -> Self {
         let buffer = vec![0; (width * height) as usize];
         let mut  window = Window::new(
             "Waza",
@@ -69,6 +97,8 @@ impl Jeu {
         });
         window.set_target_fps(fps);
         Self {
+            space_pressed : false,
+            frame_count :  0,
             width,
             height,
             buffer,
@@ -76,12 +106,18 @@ impl Jeu {
             fps,
             player,
             liste_tirs,
+            liste_ennemy,
         }
     }
 
     pub fn new_missile(&mut self) {
 
-        self.liste_tirs.push(Missile{x : self.player.x  + 20 as f32, y : self.player.y - 20 as f32});
+        self.liste_tirs.push(Missile{x : self.player.x  + 20 as f32, y : self.player.y - 20 as f32, width : 10, height : 20});
+    }
+
+    pub fn new_ennemy(&mut self){
+        let mut rng = rand::thread_rng();
+        self.liste_ennemy.push(Ennemy{x : rng.gen_range(1..=self.width) as f32, y : -20.0 , width : 16.0, height : 16.0, dy : 5.0});
     }
 
     pub fn run(&mut self){
@@ -91,26 +127,54 @@ impl Jeu {
             self.buffer = vec![0xFF0000; self.width as usize * self.height as usize];
             self.player.draw(&mut self.buffer, &self.width);
             //self.player.update(&self.window, &self.width);
+
             for tir in &self.liste_tirs {
                 tir.draw(&mut self.buffer, &self.width);
             }
+
+            for ennemy in &self.liste_ennemy{
+                ennemy.draw(&mut self.buffer, &self.width);
+            }
+
             self.window.update_with_buffer(&self.buffer, self.width as usize, self.height as usize).unwrap();
             
         }
     }
     pub fn update(&mut self){
+        self.player.dx = 0.0;
+
+        if self.frame_count % 60 == 0{
+            println!("{}",self.frame_count);
+            self.new_ennemy();
+        }
+        
         for key in self.window.get_keys() {
             match key {
-                Key::Left if (self.player.x - self.player.dx >0.0)  => self.player.x -= self.player.dx,
-                Key::Right if (self.player.x + self.player.width as f32 + self.player.dx < self.width as f32) => self.player.x += self.player.dx,
-                Key::Space => self.new_missile(),
-                _ => {}
+                Key::Left => self.player.dx = -5.0,
+                Key::Right => self.player.dx = 5.0,
+                _ => {}  // Ne rien faire pour les autres touches
             }
-        };
+        }
+        self.player.update(self.width);
+        //if (self.player.x - self.player.dx >0.0)
+        //if (self.player.x + self.player.width as f32 + self.player.dx < self.width as f32)
+        if self.window.is_key_pressed(Key::Space, KeyRepeat::No) && !self.space_pressed{
+            self.new_missile();
+            self.space_pressed = true;
+        }
+        else {
+            self.space_pressed = false;
+        }
         for tir in &mut self.liste_tirs {
             tir.y -= 5.0;
         };
-        self.liste_tirs.retain(|tir| tir.y > 0.0)
+        for ennemy in &mut self.liste_ennemy{
+            ennemy.y += 2.0;
+        }
+        self.liste_tirs.retain(|tir| tir.y > 0.0);
+        self.liste_ennemy.retain(|ennemy| ennemy.y < self.height as f32 + 10 as f32);
+        self.frame_count += 1
+
     }
 }
 
@@ -120,10 +184,10 @@ fn main() {
     let player = Vaisseau {
         x: 50.0,
         y : 300.0,
-        dx : 5.0,
+        dx : 0.0,
         width : 50,
         height : 50,
     };
-    let mut jeu_dans_main_vrai = Jeu::new(640, 360, 165, player, Vec::new());
+    let mut jeu_dans_main_vrai = Jeu::new(640, 360, 60, player, Vec::new(), Vec::new());
     jeu_dans_main_vrai.run(); 
 }
